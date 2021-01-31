12ab44b69dd4c96d93b1c3ba32fd621c85323d1f
Aleksandr Ovchinnikov <mail@mr1sunshine.com>
2021-01-31 09:41:51 +0200

    Add spotify hotkey

--- keyboards/lily58/keymaps/mr1sunshine/keymap.c
+++ keyboards/lily58/keymaps/mr1sunshine/keymap.c
@@ -41,6 +41,7 @@ enum custom_keycodes {
   RPLACE,
   VSBACK,
   ALTC,
+  SPOTIFY
 };
 
 enum { ESC_CAPS = 0, DELS };
@@ -54,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |BackSP|
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |LCTRL |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
- * |------+------+------+------+------+------| MUTE  |    |KC_MPLY|------+------+------+------+------+------|
+ * |------+------+------+------+------+------| MUTE  |    |SPOTIFY|------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |DLTROW|RPLACE|
@@ -65,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   TD(ESC_CAPS), KC_1, KC_2, KC_3,    KC_4,    KC_5,                                  KC_6,   KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS, \
   KC_TAB,       KC_Q, KC_W, KC_E,    KC_R,    KC_T,                                  KC_Y,   KC_U,    KC_I,    KC_O,   KC_P,    KC_BSPACE, \
   KC_LCTRL,     KC_A, KC_S, KC_D,    KC_F,    KC_G,                                  KC_H,   KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT, \
-  KC_LSFT,      KC_Z, KC_X, KC_C,    KC_V,    KC_B,  KC_MUTE, KC_MPLY,        KC_N,  KC_M,   KC_COMM, KC_DOT, KC_SLSH, TD(DELS), \
+  KC_LSFT,      KC_Z, KC_X, KC_C,    KC_V,    KC_B,  KC_MUTE, SPOTIFY,        KC_N,  KC_M,   KC_COMM, KC_DOT, KC_SLSH, TD(DELS), \
                             KC_LALT, KC_LGUI, LOWER, KC_SPC,  RSFT_T(KC_ENT), RAISE, DLTROW, RPLACE \
 ),
 /* LOWER
@@ -358,6 +359,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           SEND_STRING(SS_DOWN(X_LALT) SS_TAP(X_C) SS_UP(X_LALT));
       }
       break;
+    }
+    case SPOTIFY: {
+      if (record->event.pressed) {
+          SEND_STRING(SS_DOWN(X_LCTRL) SS_DOWN(X_LGUI) SS_TAP(X_S) SS_UP(X_LGUI) SS_UP(X_LCTRL));
+      }
     }
   }
   return true;
